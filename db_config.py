66cae8dfbc8528ff5ce779e5ff85811d35b2a57d
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-03-03 15:36:07 +0100

    Create Db

--- db_config.py
+++ db_config.py
@@ -1,12 +1,35 @@
 from flaskext.mysql import MySQL
+from pymysql import MySQLError
+from create_tables import create_table_students
+from populate_db import populate_db 
+
+def create_db(cursor, DB_NAME):
+  try:
+    # Create database.
+    cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+  except MySQLError as err:
+    print("Faild to create database {}".format(err))
+    exit(1)
 
 def connect_db(app):
-
   # Configure MySQL 
   app.config['MYSQL_DATABASE_USER'] = 'root'
   app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-  app.config['MYSQL_DATABASE_DB'] = 'harry-potter'
   app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+  app.config['MYSQL_DATABASE_PORT'] = 8889
 
-  mysql = MySQL(app)
-  return mysql
+  try:
+    mysql = MySQL(app)
+    cnx = mysql.connect()
+    cursor = cnx.cursor() 
+    cursor.execute("USE {}".format('harrypotter'))
+  except MySQLError as e:
+    if e.args[0] == 1049:
+      create_db(cursor, 'harrypotter')
+      cnx.select_db('harrypotter')
+      create_table_students(cursor)
+      populate_db(cursor, cnx)
+      return cnx
+  else: 
+    print("Database {} exists".format('harrypotter'))
+    return cnx
