d9a9110a222f417836c1bbe7c73e73f63e406670
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-03-03 12:53:38 +0100

    Init app

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flaskext.mysql import MySQL
+
+from db_config import connect_db
+
 app = Flask(__name__)
 
-@app.route("/")
+# Connect to DB
+cnx = connect_db(app)
+cursor = cnx.cursor()
+
+@app.route("/", methods=['GET'])
 def home():
-    return "Hello, Flask!"
+    return {"message": "Hello World!"}
+
+if __name__ == '__main__':
+  app.run()
