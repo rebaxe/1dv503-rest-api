ade50757771551c8b1c876d1c01fc0380aecb995
Kaj <kb223aw@student.lnu.se>
2022-03-07 09:30:07 +0100

    add houses table

--- create_tables.py
+++ create_tables.py
@@ -47,3 +47,28 @@ def create_table_staff(cursor):
         print(err)
     else:
         print("Successfully create table: staff.")
+        
+def create_table_houses(cursor):
+  # Define query for creating planets table.
+    create_houses = "CREATE TABLE `houses` (" \
+                      "  `house_id` int(50) NOT NULL AUTO_INCREMENT," \
+                      "  `house` varchar(255)," \
+                      "  `animal` varchar(255)," \
+                      "  `head` varchar(255)," \
+                      "  `ghost` varchar(255)," \
+                      "  `founder` varchar(255)," \
+                      "  `element` varchar(255)," \
+                      "  `first_color` varchar(255)," \
+                      "  `second_color` varchar(255)," \
+                      "  PRIMARY KEY (`house_id`)" \
+                      ")"
+
+    try:
+        print("Creating table houses: ")
+        # Create table with query.
+        cursor.execute(create_houses)
+    except MySQLError as err:
+        # Catch error if the table already existed. 
+        print(err)
+    else:
+        print("Successfully create table: houses.")
