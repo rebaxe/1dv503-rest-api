996cdac7c9f496d05da0f015979d188dc5dd6623
Kaj <kb223aw@student.lnu.se>
2022-03-07 08:18:16 +0100

    add table staff

--- create_tables.py
+++ create_tables.py
@@ -18,3 +18,22 @@ def create_table_students(cursor):
         print(err)
     else:
         print("Successfully create table: students.")
+
+def create_table_staff(cursor):
+  # Define query for creating planets table.
+    create_staff = "CREATE TABLE `staff` (" \
+                      "  `staff_id` int(50) NOT NULL AUTO_INCREMENT," \
+                      "  `name` varchar(255)," \
+                      "  `house` varchar(255)," \
+                      "  PRIMARY KEY (`staff_id`)" \
+                      ")"
+
+    try:
+        print("Creating table staff: ")
+        # Create table with query.
+        cursor.execute(create_staff)
+    except MySQLError as err:
+        # Catch error if the table already existed. 
+        print(err)
+    else:
+        print("Successfully create table: staff.")
